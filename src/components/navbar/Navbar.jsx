cebb712a4ebce044117c58a1bcf260ebd5287140
Piyal Shah <piyalsha007@gmail.com>
2025-07-08 00:48:13 +0600

    chadcn UI added successfully

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Navbar = () => {
   return (
     <div>
-        <h1>Navbar</h1>
+      <h1>Navbar</h1>
     </div>
   )
 }
